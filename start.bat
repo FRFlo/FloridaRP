de9b306c7be11f5eb2710baf2e666a18c41e4659
FRFlo <62435410+FRFlo@users.noreply.github.com>
2023-06-23 12:45:08 +0200

    feat: Ajout start.bat

---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+rem FiveM Server Start Script. Artifacts are saved in the artifacts folder and resources are saved in the resources folder.
+
+start artifacts\FXServer.exe +exec server.cfg
